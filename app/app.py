fd109ea9a3f5e0ac51c72005477886ed997650dc
Cezar Mihai Draghici <cmdraghici.92@gmail.com>
2021-05-31 11:50:44 +0200

    Added the html templates

--- app/app.py
+++ app/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask import render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World"
+	return render_template('master.html')
 
+@app.route('/go')
+def go():
+	query = request.args.get('query', '')
+	return render_template('go.html', query=query)
 
 def main():
     app.run(host='0.0.0.0', port=3001, debug=True)
